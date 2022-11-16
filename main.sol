60b8b7b610b459186d667f2ea0d577e490e4525f
faryne <faryne@gmail.com>
2022-11-16 17:25:32 +0800

    修改合約內容

--- main.sol
+++ main.sol
@@ -3,54 +3,54 @@ pragma solidity ^0.8.17;
 
 // 範例合約：實作一個含有轉帳與自毀的合約
 contract Main {
-    // 總池內有一百萬枚代幣
-    uint public totalNum = 1000000;
+    // 設定一組 map ，以 address 為 key ，記錄每個 address 的 eth 餘額
+    mapping(address => uint) private balances;
 
-    // 設定一組 map ，以 address 為 key ，記錄每個 address 的餘額
-    mapping(address => uint) balances;
+    // 設定一組 map ，以 address 為 key ，記錄每個 address 的代幣餘額
+    mapping(address => uint) private holdTokens;
+
+    // msg.value 乘以此倍數即為擁有的代幣數目
+    uint private times = 1000;
 
     // 在建構子內取得該 sender 的餘額
     constructor() payable {
         balances[msg.sender] = msg.value;
-    }
-
-    // getter：取得目前代幣總數。可供公開呼叫。
-    function getTotal() public view returns(uint) {
-        return totalNum;
+        holdTokens[msg.sender] = msg.value * times;
     }
 
     // getter：傳回指定位址目前的餘額，addr 是 address 型態資料
-    function getSender(address addr) public view returns (uint) {
+    function getSenderValue(address addr) public view returns (uint) {
         return balances[addr];
     }
 
-    function add(address to, uint amount) public payable returns (bool) {
-        balances[to] += amount;
-        return true;
+    // getter：傳回指定位址目前的代幣數目，addr 是 address 型態資料
+    function getSenderTokens(address addr) public view returns (uint) {
+        return holdTokens[addr];
     }
 
-    function minus(address to, uint amount) public payable returns (bool) {
+    // 在指定位址加上金額
+    function add(address to, uint amount) public {
+        balances[to] += amount;
+        holdTokens[to] += amount * times;
+    }
+
+    // 在指定位址減掉金額
+    function minus(address to, uint amount) public {
+        require(balances[to] > amount, unicode"餘額需大於付款數");
         balances[to] -= amount;
-        return true;
+        holdTokens[to] -= amount * times;
+    }
+
+    // 呼叫者往自己的位址存入金額
+    function Save(uint amount) public payable {
+        add(msg.sender, amount);
     }
 
     // Action：轉帳
-    function Transfer(address to, uint amount) public payable returns (bool) {
-        require(balances[msg.sender] > amount);
+    function Transfer(address to, uint amount) public payable {
         // 從轉帳方扣
         minus(msg.sender, amount);
         // 接收方加上
         add(to, amount);
-
-        return true;
     }
-
-    // Action：自毀 N 個代幣，連帶導致池內的代幣總量減少
-    function Destroy(uint amount) public returns (bool) {
-        require(balances[msg.sender] > amount);
-        minus(msg.sender, amount);
-        totalNum -= amount;
-        return true;
-    }
-
 }
